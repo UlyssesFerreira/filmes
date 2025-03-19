45f177a725d70b9e13edff9c31a55d8fa6d58b60
ulysses <ulyssesh.20@gmail.com>
2025-03-19 20:20:16 -0300

    filmes assistidos

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   devise_for :users
+  get "my_movies", to: "watched_movies#index"
+
   get "up" => "rails/health#show", as: :rails_health_check
   mount Sidekiq::Web => "/sidekiq"
 
   root "home#index"
   resources :movies do
-  resource :watched_movie
+    resource :watched_movie
   end
 end
