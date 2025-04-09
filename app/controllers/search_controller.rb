9097dd28c9ca654ca9ca1de194ab382ccbe3de71
ulysses <ulyssesh.20@gmail.com>
2025-04-09 20:51:06 -0300

    variavel search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @q = Movie.ransack(title_or_original_title_cont: params[:query], downloaded: true, order_by_rating: true)
-    @movies = @q.result.limit(8)
+    @search = Movie.ransack(title_or_original_title_cont: params[:query], downloaded: true, order_by_rating: true)
+    @movies = @search.result.limit(8)
   end
 end
