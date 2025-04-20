820bb590ac7a53a4080553060de94a8b312b475f
ulysses <ulyssesh.20@gmail.com>
2025-04-20 12:31:55 -0300

    home movies

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @best_rating_movies =  Movie.where.not(title: nil, original_language: "en").where("release_date > '2024-01-01'").order("RANDOM()").limit(16)
+    @movies =  Movie.where("release_date > '2024-01-01' AND runtime > 100 AND rating < 8").order(rating: :desc).limit(16)
   end
 end
