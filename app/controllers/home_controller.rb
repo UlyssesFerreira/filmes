1377ced646ddfe05c9a0927ec43557c71c789621
ulysses <ulyssesh.20@gmail.com>
2025-04-20 12:35:03 -0300

    movie ids

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @movies =  Movie.where("release_date > '2024-01-01' AND runtime > 100 AND rating < 8").order(rating: :desc).limit(16)
+    home_movie_ids = Movie.where("release_date > '2024-01-01' AND runtime > 100 AND rating < 8").order(rating: :desc).limit(16).pluck(:id)
+    @movies = Movie.where(id: home_movie_ids)
   end
 end
