6d014e4b4e11187bab0124512d2236c34e15f6e3
ulysses <ulyssesh.20@gmail.com>
2025-05-12 23:11:18 -0300

    validates em watched_movie

--- app/models/watched_movie.rb
+++ app/models/watched_movie.rb
@@ -1,4 +1,7 @@
 class WatchedMovie < ApplicationRecord
   belongs_to :movie
   belongs_to :user
+
+  validates :rating, comparison: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+  validates :watched_at, comparison: { less_than_or_equal_to: DateTime.now }
 end
