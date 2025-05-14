78df1b72a7832e53c519b7b1e53991f20558f1e3
ulysses <ulyssesh.20@gmail.com>
2025-05-13 23:18:42 -0300

    testes para user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]  
 
+  validates :name, presence: true
+
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(email: data['email']).first
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context "validações" do
+    it "é válido" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it "é inválido sem name" do
+      user = build(:user, name: nil)
+      expect(user).not_to be_valid
+    end
+
+    it "é inválido sem email" do
+      user = build(:user, email: nil)
+      expect(user).not_to be_valid
+    end
+
+    it "é inválido com senha menor que 6 caracteres" do
+      user = build(:user, password: "123")
+      expect(user).not_to be_valid
+    end
+
+    it "é inválido com email duplicado" do
+      create(:user)
+      user = build(:user)
+      expect(user).not_to be_valid
+    end
+
+    it "é inválido com email mal formatado" do
+      user = build(:user, email: "email_invalido")
+      expect(user).not_to be_valid
+    end
+  end
+
+  # context "comportamento" do
+  #   it "autentica com senha certa" do
+  #     user = create(:user, password: "senhacerta123")
+  #     expect(user.authenticate("senhacerta123")).to be_valid
+  #   end
+  # end
+end
