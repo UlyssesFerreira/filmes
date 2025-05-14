8d29edb2c8063c06b726470417514336fc2e142b
ulysses <ulyssesh.20@gmail.com>
2025-05-13 21:42:07 -0300

    tirando validação para review repetida

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -38,7 +38,6 @@ class Movie < ApplicationRecord
   end
 
   def mark_as_watched(user:, watched_at: DateTime.now, rating: nil, comment: "")
-    return false if already_on_watched_list?(user)
     watched_movies.new(
       user_id: user.id,
       watched_at: watched_at,
