c3335b420aee1d174bc191eedccb97e6bb63edf2
ulysses <ulyssesh.20@gmail.com>
2025-05-17 12:39:17 -0300

    testes watched movie

--- spec/models/watched_movie_spec.rb
+++ spec/models/watched_movie_spec.rb
@@ -5,6 +5,27 @@ RSpec.describe WatchedMovie, type: :model do
     user = create(:user)
     movie = create(:movie)
     review = movie.mark_as_watched(user: user, watched_at: Date.yesterday, rating: 10, comment: "Bom")
-    expect(review.save).to be true
+    expect(review.save).to be_truthy
+  end
+
+  it "é inválido registrar um filme com nota menor que 1" do
+    user = create(:user)
+    movie = create(:movie)
+    review = movie.mark_as_watched(user: user, watched_at: Date.yesterday, rating: -1)
+    expect(review.save).to be_falsey
+  end
+
+  it "é inválido registrar um filme com nota maior que 10" do
+    user = create(:user)
+    movie = create(:movie)
+    review = movie.mark_as_watched(user: user, watched_at: Date.yesterday, rating: 15)
+    expect(review.save).to be_falsey
+  end
+
+  it "é inválido registrar um filme em uma data futura" do
+    user = create(:user)
+    movie = create(:movie)
+    review = movie.mark_as_watched(user: user, watched_at: Date.tomorrow, rating: 10)
+    expect(review.save).to be_falsey
   end
 end
